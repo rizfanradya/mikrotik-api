67f2e8c31e85ef2349a404a0a35313e8bfa10515
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2023-12-29 15:12:29 +0000

    login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,31 +2,39 @@ import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
 
 export default function Login() {
   return (
-    <Card className="max-w-sm">
-      <form className="flex flex-col gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email1" value="Your email" />
+    <div className="h-screen p-6">
+      <Card className="max-w-sm m-auto translate-y-2/4">
+        <p className="text-slate-700 text-center font-semibold text-2xl">
+          LOGIN
+        </p>
+        <form className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email1" value="Username" />
+            </div>
+            <TextInput
+              id="email1"
+              type="text"
+              placeholder="Username"
+              required
+            />
           </div>
-          <TextInput
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password1" value="Your password" />
+
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password1" value="Password" />
+            </div>
+            <TextInput
+              id="password1"
+              type="password"
+              placeholder="Password"
+              required
+            />
           </div>
-          <TextInput id="password1" type="password" required />
-        </div>
-        <div className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Label htmlFor="remember">Remember me</Label>
-        </div>
-        <Button type="submit">Submit</Button>
-      </form>
-    </Card>
+
+          <Button type="submit">Login</Button>
+        </form>
+      </Card>
+    </div>
   );
 }
