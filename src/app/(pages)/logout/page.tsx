d52f53fbde761166f725b2283066b8f2a8581df2
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-01-05 03:29:17 +0000

    loading

--- src/app/(pages)/logout/page.tsx
+++ src/app/(pages)/logout/page.tsx
@@ -1,5 +1,8 @@
 import Navbar from "@/app/navbar";
+import { redirect } from "next/navigation";
 
 export default function Logout() {
+  redirect("/login");
+
   return <Navbar title="Logout">Logout</Navbar>;
 }
