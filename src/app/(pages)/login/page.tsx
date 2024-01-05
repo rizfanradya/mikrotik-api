29034fa0b385a1d2f89a67be2df88c1277512906
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-01-05 03:56:12 +0000

    login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,34 @@
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+"use client";
+import { Card, Label, TextInput } from "flowbite-react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  username: string;
+  password: string;
+};
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const handleLogin: SubmitHandler<Inputs> = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className="h-screen p-6">
       <Card className="max-w-sm m-auto translate-y-2/4">
         <p className="text-slate-700 text-center font-semibold text-2xl">
           LOGIN
         </p>
-        <form className="flex flex-col gap-4">
+
+        <form
+          onSubmit={handleSubmit(handleLogin)}
+          className="flex flex-col gap-4"
+        >
           <div>
             <div className="mb-2 block">
               <Label htmlFor="email1" value="Username" />
@@ -17,6 +38,7 @@ export default function Login() {
               type="text"
               placeholder="Username"
               required
+              {...register("username", { required: true })}
             />
           </div>
 
@@ -29,10 +51,13 @@ export default function Login() {
               type="password"
               placeholder="Password"
               required
+              {...register("password", { required: true })}
             />
           </div>
 
-          <Button type="submit">Login</Button>
+          <button className="btn btn-info" type="submit">
+            Login
+          </button>
         </form>
       </Card>
     </div>
