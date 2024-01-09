f98d39356077c56de8f1faf0fe8d4e6e7844caa0
rizfanradya <rizfankusuma@gmail.com>
2024-01-10 01:15:57 +0700

    delete

--- src/app/(pages)/routerSettings/main.tsx
+++ src/app/(pages)/routerSettings/main.tsx
@@ -2,13 +2,12 @@
 import Navbar from "@/app/navbar";
 import { retrieveData } from "@/utils/retrieveData";
 import Link from "next/link";
-import randomColor from "randomcolor";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { FaServer, FaExternalLinkAlt, FaEdit } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
+import { FaServer } from "react-icons/fa";
+import { CiMenuKebab } from "react-icons/ci";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useState } from "react";
-import { doc, setDoc } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "@/utils/firebase";
 
 type Inputs = {
@@ -22,6 +21,8 @@ export default function RouterSettingsLayout() {
   const { register, handleSubmit } = useForm<Inputs>();
   const [seePassword, setSeePassword] = useState<boolean>(false);
   const [buttonSubmit, setButtonSubmit] = useState<boolean>(false);
+  const [menuData, setMenuData] = useState<any>(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const onSubmit: SubmitHandler<Inputs> = async (e) => {
     setButtonSubmit(true);
@@ -46,48 +47,102 @@ export default function RouterSettingsLayout() {
     }
   };
 
+  const deleteData = async (
+    id: string,
+    hotspotName: string,
+    sessionName: string
+  ) => {
+    const confirmDelete = confirm(
+      `apakah anda yakin ingin mengahpus data :\nhotspot name : ${hotspotName}\nsession name : ${sessionName}\n???`
+    );
+    if (confirmDelete) {
+      await deleteDoc(doc(db, "router", id));
+    } else {
+      alert(`data ${hotspotName} batal dihapus!!`);
+    }
+  };
+
+  const toggleMenu = (id: string) => {
+    setMenuData((prevId: any) => (prevId === id ? null : id));
+  };
+  const handleClickOutside = (event: any) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuData(null);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <Navbar title="Router Settings">
       <div className="md:flex">
         {/* router list */}
-        <div className="border-primary border-2 overflow-hidden rounded-lg bg-base-200 w-full mb-4 md:mb-0 md:mr-4">
+        <div className="border-primary border-2 rounded-lg bg-base-200 w-full mb-4 md:mb-0 md:mr-4">
           <h1 className="bg-primary p-1 font-semibold text-white">
             Router List
           </h1>
 
           {dataRouter ? (
-            <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
-              {dataRouter.map((doc: any) => (
+            <div
+              ref={menuRef}
+              className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-3 m-3"
+            >
+              {dataRouter.map((doc: any, index: any) => (
                 <div
                   key={doc.id}
-                  style={{
-                    backgroundColor: randomColor({ luminosity: "bright" }),
-                  }}
-                  className="flex gap-4 items-center m-2 rounded-lg p-3 text-white"
+                  className="flex gap-4 items-center rounded-lg p-3 text-white bg-slate-700 relative"
                 >
-                  <div className="w-16">
-                    <FaServer size="4em" />
+                  <div className="absolute top-0 left-0 badge badge-success">
+                    {index + 1}
                   </div>
+
+                  <div className="w-14">
+                    <FaServer size="3em" />
+                  </div>
+
                   <div>
-                    <div className="text-sm">
+                    <div className="text-xs grid gap-2">
                       <p>Hotspot Name : {doc.hotspotName}</p>
                       <p>Session Name : {doc.sessionName}</p>
                     </div>
 
-                    <div className="flex gap-2">
-                      <Link className="badge badge-accent" href={"/"}>
-                        <FaExternalLinkAlt />
-                        Open
-                      </Link>
-                      <Link className="badge badge-primary" href={"/"}>
-                        <FaEdit />
-                        Edit
-                      </Link>
-                      <Link className="badge-error badge" href={"/"}>
-                        <MdDelete />
-                        Delete
-                      </Link>
+                    <div
+                      onClick={() => toggleMenu(doc.id)}
+                      className="absolute top-0 right-0 bg-slate-800 p-2 rounded-bl-3xl rounded-tr-lg cursor-pointer"
+                    >
+                      <CiMenuKebab size="1.2em" />
                     </div>
+
+                    {menuData === doc.id && (
+                      <div className="absolute top-0 right-8 bg-slate-800 rounded-lg z-10">
+                        <ul className="font-light text-[10px] menu">
+                          <li>
+                            <Link href={"/"}>Connect</Link>
+                          </li>
+                          <li>
+                            <Link href={"/"}>Ping</Link>
+                          </li>
+                          <li>
+                            <Link href={"/"}>Edit</Link>
+                          </li>
+                          <li
+                            onClick={() =>
+                              deleteData(
+                                doc.id,
+                                doc.hotspotName,
+                                doc.sessionName
+                              )
+                            }
+                          >
+                            <span>Delete</span>
+                          </li>
+                        </ul>
+                      </div>
+                    )}
                   </div>
                 </div>
               ))}
